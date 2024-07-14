c8f2b738a87744afcc5774650316ceb83322ff4e
yasmine <guerricheyasmine@gmail.com>
2024-07-14 08:28:49 +0200

    ajout de la page fiche apartment

--- kasa/src/components/ApartmentInfos/index.jsx
+++ kasa/src/components/ApartmentInfos/index.jsx
@@ -2,15 +2,31 @@ import './ApartmentInfos.scss';
 
 function ApartmentInfos() {
     return (
-        <div className='apartment-info'>
+        <div className="apartment-header">
+            <div className='apartment-info'>
                 <h1 >Crazy loft on the canal Saint Martin</h1>
                 <h2 >Paris, ile de France</h2>  
-                    <p>Cozy</p>
-                    <p>Canal</p>
-                    <p>Paris 10</p>
-           </div>
+                    <div className='apartment-tags'>
+                        <span>Cozy</span>
+                        <span>Canal</span>
+                        <span>Paris 10</span>
+                    </div> 
+            </div>
+            <div className='apartment-details__owner'>
+                <div className="apartment-details">
+                <h3><span>Alexandre</span><span>Dumas</span></h3>
+            <div className='apartment-details__badge'></div>
+            </div>
+                <div className="apartment-stars">
+                    <span className='star-on'>★</span>
+                    <span className='star-on'>★</span>
+                    <span className='star-on'>★</span>
+                    <span className='star-off'>★</span>
+                    <span className='star-off'>★</span>
+                </div>
+            </div>
+        </div>
 
     )
 }
-
 export default ApartmentInfos
--- kasa/src/pages/apartement-page/index.jsx
+++ kasa/src/pages/apartement-page/index.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import Main from '../../components/main';
 import ApartmentBanner from '../../components/ApartmentBanner';
 import ApartmentInfos from '../../components/ApartmentInfos';
-import ApartmentRating from '../../components/ApartmentRating';
 import ApartmentDetails from '../../components/ApartmentDetails';
+import './apartment-page.scss';
 
 function FicheAppartement() {
     return (
         <Main>
             <ApartmentBanner />
             <ApartmentInfos />
-            <ApartmentRating />
+            <div className='apartment-details__container'>
             <ApartmentDetails />
             <ApartmentDetails />
+            </div>
         </Main>
     );
 }
